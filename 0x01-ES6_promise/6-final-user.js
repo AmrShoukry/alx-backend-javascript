85aa74ccf238641917d86499f283ad79e989fae0
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-28 18:22:29 +0200

    task6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,15 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise0 = signUpUser(firstName, lastName);
   const promise1 = uploadPhoto(fileName);
 
   return Promise.allSettled([promise0, promise1])
-    .then((responses) => {
-      return responses.map((response) => ({
-        status: response.status,
-        value:
-          response.status === "fulfilled" ? response.value : response.reason,
-      }));
-    })
+    .then((responses) => responses.map((response) => ({
+      status: response.status,
+      value:
+          response.status === 'fulfilled' ? response.value : response.reason,
+    })))
     .catch();
 }
