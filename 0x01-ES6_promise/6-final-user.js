facc3753e44d9417cba41191fb82a3f5bc0f3331
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-28 18:24:10 +0200

    task6 fix eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,17 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise0 = signUpUser(firstName, lastName);
   const promise1 = uploadPhoto(fileName);
 
   return Promise.allSettled([promise0, promise1])
-    .then((responses) =>
-      responses.map((response) => ({
-        status: response.status,
-        value:
-          response.status === "fulfilled"
+    .then((responses) => responses.map((response) => ({
+      status: response.status,
+      value:
+          response.status === 'fulfilled'
             ? response.value
             : String(response.reason),
-      }))
-    )
+    })))
     .catch();
 }
