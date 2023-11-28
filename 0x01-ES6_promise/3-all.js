5cabe640e40f3df2d1961272ea649f7eb59b509a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-28 17:47:14 +0200

    task3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise0 = uploadPhoto();
+  const promise1 = createUser();
+
+  Promise.all([promise0, promise1])
+    .then((responses) => {
+      console.log(
+        `${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`,
+      );
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
