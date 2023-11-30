e41d202dd7f658698f3924646c7c54ab8b171b9f
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-30 21:21:31 +0200

    task4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
+    if (!(currency instanceof Currency)) throw new TypeError('Currency must be a currency');
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') throw new TypeError('Amount must be a number');
+
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) throw new TypeError('Currency must be a currency');
+
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
+    if (typeof conversionRate !== 'number') throw new TypeError('Conversion Rate must be a number');
+
+    return amount * conversionRate;
+  }
+}
