37fe484b9cadccea621e0d58ee5604722c4f97c2
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-30 21:38:19 +0200

    task5 trial

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
