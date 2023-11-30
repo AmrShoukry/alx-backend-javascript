6a61312fc2f825fc5113b7956b2f7b7021d70159
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-30 21:54:56 +0200

    task7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,37 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof code !== 'string') throw new TypeError('Code must be a string');
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+
+    this._name = value;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw new TypeError('Code must be a string');
+
+    this._code = value;
+  }
+
+  valueOf() {
+    return `Airport [${this._code}] { _name: ${this._name}, _code: ${this._code} }`;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
