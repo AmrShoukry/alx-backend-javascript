7b47447534255f41c3152dbe063dc05b2f621922
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-30 21:06:45 +0200

    task2 fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,6 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (typeof students !== 'object') throw new TypeError('Students must be an array of Strings');
 
     if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of Strings');
@@ -26,14 +25,20 @@ export default class HolbertonCourse {
   }
 
   set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
     this._name = value;
   }
 
   set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
     this._length = value;
   }
 
   set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of Strings');
+    }
+
     this._students = value;
   }
 }
