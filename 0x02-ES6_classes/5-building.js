2ae79687e35532c1dd07d2212ab4fb920201d1f8
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-11-30 21:34:56 +0200

    task5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') throw new TypeError('Sqft must be a number');
+
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(`${this._sqft} Class extending Building must override evacuationWarningMessage`);
+  }
+}
