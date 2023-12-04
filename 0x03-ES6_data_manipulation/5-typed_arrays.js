8a5760849406cc932d8436b76bb85bf71d9917b7
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-04 21:30:01 +0200

    task5 fix4

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const dv = new DataView(buffer, 0, length);
-
   if (position < 0 || position >= length) throw new Error('Position outside range');
 
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer, 0, length);
   dv.setInt8(position, value);
 
   return buffer;
