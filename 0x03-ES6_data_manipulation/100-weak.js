c795120f8c8ed3f32b3a37df013fc43f86593310
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-04 22:26:42 +0200

    task100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const bool = weakMap.has(endpoint);
+  if (bool === false) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const times = weakMap.get(endpoint);
+    if (times >= 5) {
+      throw new Error('Endpoint load is high');
+    } else {
+      weakMap.set(endpoint, times + 1);
+    }
+  }
+}
+
+export { weakMap };
