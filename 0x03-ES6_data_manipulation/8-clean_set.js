cb486daac9a86d1cd81d1a3dc8da9c37f13953eb
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-04 22:03:09 +0200

    task8 fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || !set) return '';
   const stringArray = [];
   const index = startString.length;
 
