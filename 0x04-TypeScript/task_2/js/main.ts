e37619765e1a0d688b8a63f60d88a71736844cd7
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-06 23:03:30 +0200

    task2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,55 @@
+interface DirectorInterface {
+  workFromHome: () => string;
+  getCoffeeBreak: () => string;
+  workDirectorTasks: () => string;
+}
+
+interface TeacherInterface {
+  workFromHome: () => string;
+  getCoffeeBreak: () => string;
+  workTeacherTasks: () => string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome: () => string;
+  getToWork: () => string;
+  workDirectorTasks: () => string;
+  getCoffeeBreak: () => string;
+
+  constructor() {
+    this.workFromHome = function (): string {
+      return "Working from home";
+    };
+    this.getCoffeeBreak = function () {
+      return "Cannot have a break";
+    };
+    this.workDirectorTasks = function () {
+      return "Getting to director tasks";
+    };
+    this.getToWork = function () {
+      return "Getting a coffee break";
+    };
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome: () => string;
+  getCoffeeBreak: () => string;
+  workTeacherTasks: () => string;
+  constructor() {
+    this.workFromHome = function () {
+      return "Cannot work from home";
+    };
+    this.getCoffeeBreak = function () {
+      return "Cannot have a break";
+    };
+    this.workTeacherTasks = function () {
+      return "Getting to work";
+    };
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) return new Teacher();
+  return new Director();
+}
